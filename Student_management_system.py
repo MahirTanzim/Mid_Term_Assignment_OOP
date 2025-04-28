ec94c563de1741363172523bc99f4993f9915127
MahirTanzim <tanzim15-5400@diu.edu.bd>
2025-04-29 03:20:17 +0600

    problem 1 to  5

---
+++ Student_management_system.py
@@ -0,0 +1,42 @@
+class StudentDatabase:
+    student_list = []  
+
+    @classmethod
+    def add_student(self, student_data):
+        self.student_list.append(student_data)
+
+
+class Student:
+    def __init__(self, student_id, name, department, is_enrolled):
+        self._student_id = student_id
+        self._name = name
+        self._department = department
+        self._is_enrolled = is_enrolled
+        student_data = {
+            'student_id': self._student_id,
+            'name': self._name,
+            'department': self._department,
+            'is_enrolled': self._is_enrolled
+        }
+        StudentDatabase.add_student(student_data)
+
+
+        def enroll_student(self):
+            if self._is_enrolled:
+                print(f"Student {self._student_id} is already enrolled.")
+            else:
+                self._is_enrolled = True
+                print(f"Student {self._student_id} has been enrolled.")
+        
+        def drop_student(self):
+            if not self._is_enrolled:
+                print(f"Student {self._student_id} is already Droped.")
+            else:
+                self._is_enrolled = False
+                print(f"Student {self._student_id} has been dropped.")
+
+
+
+
+st1 = Student(101, "Mahir", "CSE", True)
+print(StudentDatabase.student_list[0]["name"])
